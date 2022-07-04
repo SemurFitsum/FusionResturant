814dfdd5245f891339cdbe7f64acff4df277fed9
Semur Fitsum <semur.fitsum@gmail.com>
2022-07-04 08:32:34 +0300

    Add project files.

---
+++ Fusion.ServicesProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Fusion.ServicesProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fusion.ServicesProductAPI.DbContexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Fusion.ServicesProductAPI/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Fusion.ServicesProductAPI.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Configuration;
+
+namespace Fusion.ServicesProductAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fusion.ServicesProductAPI", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+    }
+}
