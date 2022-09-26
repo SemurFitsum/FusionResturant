2b8249124a153a5e3e910aa6b8e8ff93f550518b
Semur Fitsum <semur.fitsum@gmail.com>
2022-09-26 07:57:55 +0300

    Create Coupon API Service

---
+++ Fusion.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace Fusion.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                //config.CreateMap<Product, ProductDTO>().ReverseMap();
+                //config.CreateMap<CartHeaderDTO, CartHeader>().ReverseMap();
+                //config.CreateMap<CartDetails, CartDetailsDTO>().ReverseMap();
+                //config.CreateMap<Cart, CartDTO>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
---
+++ Fusion.Services.CouponAPI/Program.cs
@@ -0,0 +1,19 @@
+using Fusion.Services.CouponAPI;
+
+namespace Fusion.CouponAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
+    }
+}
