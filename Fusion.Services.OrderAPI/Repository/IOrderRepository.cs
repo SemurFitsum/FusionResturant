0e1f32b328f86716775c636d768701906496a220
Semur Fitsum <semur.fitsum@gmail.com>
2022-10-19 07:34:13 +0300

    Order API - Order Repository Interface

---
+++ Fusion.Services.OrderAPI/Repository/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using Fusion.Services.OrderAPI.Models;
+
+namespace Fusion.Services.OrderAPI.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<bool> AddOrder(OrderHeader orderHeader);
+        Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid);
+    }
+}
---
+++ Fusion.Services.OrderAPI/Repository/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using Fusion.Services.OrderAPI.Models;
+
+namespace Fusion.Services.OrderAPI.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public Task<bool> AddOrder(OrderHeader orderHeader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
