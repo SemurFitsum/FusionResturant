ccbf2cb1e29f42bfcd68b915ea2d29461d82aee6
Semur Fitsum <semur.fitsum@gmail.com>
2022-10-20 08:10:51 +0300

    Order API - Process Checkout Message and Create Order Details in Database

--- Fusion.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
+++ Fusion.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -1,4 +1,6 @@
 ﻿using Azure.Messaging.ServiceBus;
+using Fusion.Services.OrderAPI.Models;
+using Fusion.Services.OrderAPI.Repository;
 using Newtonsoft.Json;
 using System.Text;
 
@@ -6,6 +8,13 @@ namespace Fusion.Services.OrderAPI.Messaging
 {
     public class AzureServiceBusConsumer
     {
+        private readonly OrderRepository _orderRepository;
+
+        public AzureServiceBusConsumer(OrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
         private async Task OnCheckOutMessageReceived(ProcessMessageEventArgs args)
         {
             var message = args.Message;
@@ -14,7 +23,38 @@ namespace Fusion.Services.OrderAPI.Messaging
 
             CheckoutHeaderDTO checkoutHeaderDTO = JsonConvert.DeserializeObject<CheckoutHeaderDTO>(body);
 
+            OrderHeader orderHeader = new()
+            {
+                UserId = checkoutHeaderDTO.UserId,
+                FirstName = checkoutHeaderDTO.FirstName,
+                LastName = checkoutHeaderDTO.LastName,
+                OrderDetails = new List<OrderDetails>(),
+                CardNumber = checkoutHeaderDTO.CardNumber,
+                CouponCode = checkoutHeaderDTO.CouponCode,
+                CVV = checkoutHeaderDTO.CVV,
+                DiscountTotal = checkoutHeaderDTO.DiscountTotal,
+                Email = checkoutHeaderDTO.Email,
+                ExpiryMonthYear = checkoutHeaderDTO.ExpiryMonthYear,
+                OrderTime = DateTime.Now,
+                OrderTotal = checkoutHeaderDTO.OrderTotal,
+                PaymentStatus = false,
+                PickupDateTime = checkoutHeaderDTO.PickupDateTime
+            };
 
+            foreach (var detailList in checkoutHeaderDTO.cartDetails)
+            {
+                OrderDetails orderDetails = new()
+                {
+                    ProductId = detailList.ProductId,
+                    ProductName = detailList.Product.Name,
+                    Price = detailList.Product.Price,
+                    Count = detailList.Count
+                };
+                orderHeader.CartTotalItems += detailList.Count;
+                orderHeader.OrderDetails.Add(orderDetails);
+            }
+
+            await _orderRepository.AddOrder(orderHeader);
         }
     }
 }
