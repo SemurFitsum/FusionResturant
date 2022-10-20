1a617d684e2751992fb79dfb33148a8db79d1ef9
Semur Fitsum <semur.fitsum@gmail.com>
2022-10-20 07:52:39 +0300

    Order API - Create Models for  Azure Messaging

---
+++ Fusion.Services.OrderAPI/Messages/CartDetailsDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fusion.Services.OrderAPI.Messaging
+{
+    public class CartDetailsDTO
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public int ProductId { get; set; }
+        public virtual ProductDTO Product { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Fusion.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Fusion.Services.OrderAPI.Messaging
+{
+    public class AzureServiceBusConsumer
+    {
+        private async Task OnCheckOutMessageReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CheckoutHeaderDTO checkoutHeaderDTO = JsonConvert.DeserializeObject<CheckoutHeaderDTO>(body);
+
+
+        }
+    }
+}
