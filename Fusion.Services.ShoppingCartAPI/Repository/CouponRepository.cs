bb0521d0529a93da5a316563dfbda85a12b61f81
Semur Fitsum <semur.fitsum@gmail.com>
2022-10-29 11:32:58 +0300

    Sync Microservice Communication - Create Coupon Repository Interface

---
+++ Fusion.Services.ShoppingCartAPI/Repository/CouponRepository.cs
@@ -0,0 +1,12 @@
+﻿using Fusion.Services.ShoppingCartAPI.Models.DTO;
+
+namespace Fusion.Services.ShoppingCartAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        public Task<CouponDTO> GetCoupon(string couponName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Fusion.Services.ShoppingCartAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿using Fusion.Services.ShoppingCartAPI.Models.DTO;
+
+namespace Fusion.Services.ShoppingCartAPI.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDTO> GetCoupon(string couponName);
+    }
+}
