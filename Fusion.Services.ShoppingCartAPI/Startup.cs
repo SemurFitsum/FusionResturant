56af76d218ca74db9997d44bb422f004266c4cf6
Semur Fitsum <semur.fitsum@gmail.com>
2022-10-29 12:44:17 +0300

    Sync Microservice Communication - Configure Start up file for Coupon Repository

--- Fusion.Services.ShoppingCartAPI/Startup.cs
+++ Fusion.Services.ShoppingCartAPI/Startup.cs
@@ -27,8 +27,13 @@ namespace Fusion.Services.ShoppingCartAPI
             services.AddSingleton(mapper);
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<ICartRepository, CartRepository>();
+            services.AddScoped<ICouponRepository, CouponRepository>();
+
             services.AddSingleton<IMessageBus, AzureServiceBusMessageBus>();
             services.AddControllers();
+
+            services.AddHttpClient<ICouponRepository, CouponRepository>(u => u.BaseAddress =
+                new Uri(Configuration["ServiceUrls:CouponAPI"]));
 
             services.AddAuthentication("Bearer")
                 .AddJwtBearer("Bearer", options =>
