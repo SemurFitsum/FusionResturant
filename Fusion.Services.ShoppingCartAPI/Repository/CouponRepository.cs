45c586d3324785da89281aa876dd22597fa7fe9b
Semur Fitsum <semur.fitsum@gmail.com>
2022-10-29 12:21:19 +0300

    Sync Microservice Communication - Implement Coupon Repository

--- Fusion.Services.ShoppingCartAPI/Repository/CouponRepository.cs
+++ Fusion.Services.ShoppingCartAPI/Repository/CouponRepository.cs
@@ -1,12 +1,27 @@
 ﻿using Fusion.Services.ShoppingCartAPI.Models.DTO;
+using Fusion.ShoppingCartAPI.Models.DTO;
+using Newtonsoft.Json;
 
 namespace Fusion.Services.ShoppingCartAPI.Repository
 {
     public class CouponRepository : ICouponRepository
     {
-        public Task<CouponDTO> GetCoupon(string couponName)
+        private readonly HttpClient client;
+
+        public CouponRepository(HttpClient client)
         {
-            throw new NotImplementedException();
+            this.client = client;
+        }
+        public async Task<CouponDTO> GetCoupon(string couponName)
+        {
+            var response = await client.GetAsync($"/api/coupon/{ couponName}");
+            var apiContent = await response.Content.ReadAsStringAsync();
+            var resp = JsonConvert.DeserializeObject<ResponseDTO>(apiContent);
+            if (resp.IsSuccess)
+            {
+                return JsonConvert.DeserializeObject<CouponDTO>(Convert.ToString(resp.Result));
+            }
+            return new CouponDTO();
         }
     }
 }
